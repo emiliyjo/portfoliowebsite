055f537bd35f78021a1454b38eb4c5acdcaa90f4
Glitch (glitch-hello-website) <none>
2023-03-09 15:28:31 +0000

    ⛪️💒 Checkpoint ./script.js:13233154/422

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-// create a new airtable object
+// activate airtable object
 var Airtable = require("airtable");
 
+// make a variable called base, which will create a new airtable object, with my api key, and specific base reference code -> both located in your airtable account
+
+var base = new Airtable(
+  {apiKey:"keyDd4ipeCQWl3r7x"}
+).base("app8mVXPpMc3jUhsk");
+
+
+
+
+console.log(base);
