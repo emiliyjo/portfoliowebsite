dd863694bdab01dc53b71f93c3c1fdafe4832093
Glitch (glitch-hello-website) <none>
2023-03-30 11:35:50 +0000

    👗👩 Checkpoint ./style.css:13233154/58 ./script.js:13233154/11465

--- script.js
+++ script.js
@@ -1,73 +1,61 @@
 // activate airtable object
 var Airtable = require("airtable");
 
-var base = new Airtable(
-  {apiKey:"keyDd4ipeCQWl3r7x"}
-).base("app8mVXPpMc3jUhsk");
+var base = new Airtable({ apiKey: "keyDd4ipeCQWl3r7x" }).base(
+  "app8mVXPpMc3jUhsk"
+);
 
-base("Table 1").select({
+base("Table 1")
+  .select({
     maxRecords: 5,
     // view: "Grid view",
-}).eachPage(
-  function page(records, fetchNextPage){
-    console.log("records:", records); 
-    records.forEach(
-      function (record) {
-        // pull my airtable data 
-        // each record will have its own div
-        let airtableItem = document.createElement("div");
-        // add some data specific meta to my new divs for filtering
-        airtableItem.classList.add("airtable-item");
-        airtableItem.setAttribute("data-mood", record.fields.Mood);
-        
-        
-        // create a img tag for my album art 
-        let albumCover = document.createElement("img");
-        albumCover.src = record.fields.AlbumCover[0].url;
-        // create a span for my artist name 
-        let artistName = document.createElement("span");
-        artistName.innerHTML = record.fields.ArtistName;
-        
-        // appending to div holding each airtable record 
-        airtableItem.append(albumCover);
-        airtableItem.append(artistName);
-       // append div to body 
-        document.body.append(airtableItem);
-      }
-    
-    )
-  }
-); 
+  })
+  .eachPage(function page(records, fetchNextPage) {
+    console.log("records:", records);
+    records.forEach(function (record) {
+      // pull my airtable data
+      // each record will have its own div
+      let airtableItem = document.createElement("div");
+      // add some data specific meta to my new divs for filtering
+      airtableItem.classList.add("airtable-item");
+      airtableItem.setAttribute("data-mood", record.fields.Mood);
 
-// set up a event listener for my empowering button 
-// listen for user clicker, once it is clicker, serach for divs with data-mood, empowering 
+      // create a img tag for my album art
+      let albumCover = document.createElement("img");
+      albumCover.src = record.fields.AlbumCover[0].url;
+      // create a span for my artist name
+      let artistName = document.createElement("span");
+      artistName.innerHTML = record.fields.ArtistName;
 
-// get our button using css ID 
+      // appending to div holding each airtable record
+      airtableItem.append(albumCover);
+      airtableItem.append(artistName);
+      // append div to body
+      document.body.append(airtableItem);
+    });
+  });
+
+// set up a event listener for my empowering button
+// listen for user clicker, once it is clicker, serach for divs with data-mood, empowering
+
+// get our button using css ID
 // assign a event listener to my button to listen for click
 let empoweringFilterBtn = document.getElementById("Empowering");
-empoweringFilterBtn.addEventListener("click", function(event){
+empoweringFilterBtn.addEventListener("click", function (event) {
   console.log("this is filter being pressed:", event.target.id);
   // search my airtable-item divs, and see which data-mood contains "empowering"
   // put my airtable-divs in an array [airtable-div 1, airtable-div-2], find the div tht has data-mood
   let listofAirtableItems = document.querySelectorAll("div.airtable-item");
-  
-  // search for data-mood, containg empowering 
-  listofAirtableItems.forEach(
-    function searchEmpoweringFilter(item){
-      // if item.dataset.mood equal to "Empowering, then we trigger something 
-      if (item.dataset.mood == "Empowering") {
-        // if the div has data-mood empowering, add red background by adding css class
-        item.classList.add("empowering-filter-show");
-        console.log(item);
-      }
-      else {
-        item.classList.add("empowering-filter-hide");
-      }
-      
-      
+
+  // search for data-mood, containg empowering
+  listofAirtableItems.forEach(function searchEmpoweringFilter(item) {
+    // if item.dataset.mood equal to "Empowering, then we trigger something
+    if (item.dataset.mood == "Empowering") {
+      // if the div has data-mood empowering, add red background by adding css class
+      item.classList.add("empowering-filter-show");
+      console.log(item);
+    } else {
+      item.classList.add("empowering-filter-hide");
     }
-  )
-  
+  });
 });
-
-
